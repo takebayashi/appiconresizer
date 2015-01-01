4d4d09582a4c366686ae413ecc887b3fd5f0dd91
Shun Takebayashi <shun@takebayashi.asia>
2015-01-01 20:53:57 +0900

    Add size settings

--- app.pl
+++ app.pl
@@ -25,16 +25,39 @@ post '/upload' => sub {
   my $zip = Archive::Zip->new;
 
   my %configs = (
-    'icon-60@3x' => 180
+    'icon-57' => 57,
+    'icon-57@2x' => 114,
+
+    'icon-60@2x' => 120,
+    'icon-60@3x' => 180,
+
+    'icon-72' => 72,
+    'icon-72@2x' => 144,
+
+    'icon-76' => 76,
+    'icon-76@2x' => 152,
+
+    'icon-29' => 29,
+    'icon-29@2x' => 58,
+    'icon-29@3x' => 87,
+
+    'icon-40@2x' => 80,
+    'icon-40@3x' => 120,
+
+    'icon-50' => 50,
+    'icon-50@2x' => 100,
+
+    'iTunesArtwork@2x' => 1024,
   );
 
-  my $image = new Image::Magick;
-  $image->Read($input_filename);
   while (my ($name, $size) = each(%configs)) {
-    my $icon_filename = $icons_dir . '/' . $name . '.png';
+    my $image = new Image::Magick;
+    $image->Read($input_filename);
+    my $icon_basename = $name . '.png';
+    my $icon_filename = $icons_dir . '/' . $icon_basename;
     $image->Scale(width => $size, height => $size);
     $image->Write('png:' . $icon_filename);
-    $zip->addFile($icon_filename);
+    $zip->addFile($icon_filename, $icon_basename);
   }
 
   my $content;
